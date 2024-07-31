8762ff119cda955af2ea7c9cc9c4d1c8ee4ab660
AhmedUIN <123808372+AUIN0@users.noreply.github.com>
2024-07-31 07:40:42 +0300

    Update guest_counter.ino

--- guest_counter.ino
+++ guest_counter.ino
@@ -1,21 +1,20 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
-// Initialize the library with the I2C address and dimensions of the display
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
-int IR1 = 2;
+int PIR = 2;
 int slot = 1;
 
 void setup() {
   lcd.init();
-  lcd.backlight();  // Turn on the backlight
-  pinMode(IR1, INPUT);
+  lcd.backlight();
+  pinMode(PIR, INPUT);
   Serial.begin(9600);
 }
 
 void loop() {
-  if (digitalRead(IR1) == HIGH) {
+  if (digitalRead(PIR) == HIGH) {
     if (slot > 0) {
       slot++;
       lcd.setCursor(0, 0);
@@ -26,7 +25,7 @@ void loop() {
       Serial.print("Detected: ");
       Serial.println(slot);
     }
-    else if(digitalRead(IR1)==LOW){
+    else if(digitalRead(PIR)==LOW){
       lcd.print(slot);
     }
     delay(5000);
